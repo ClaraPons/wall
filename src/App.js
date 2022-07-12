8950d210159f1c774f27277408b7a1c410622cd0
Clara Pons <clarapons@macbook-air-de-clara.home>
2022-07-12 12:30:10 +0200

    all ex without css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from 'react'
 import users from './users.json'
 import Card from './components/Card'
 
+
 class App extends React.Component {
   render() {
+
     return (
-      // hello
+      <div className='container'>
+      <h1>The Wall</h1>
+
+      <section className='row'>
+      {users.map(user => {
+        return (
+        <Card
+          name = {user.name}
+          picture = {user.picture}
+          about = {user.about}
+        />
+        )
+      })}
+      </section>
+      </div>
     )
-  }
+  } 
 }
 
 export default App 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import users from '../users.json'
+
 
 class Card extends React.Component {
   render() {
     return (
-      // hello
+      <div className="Card">
+        <h2>{this.props.name}</h2>
+        <img src={this.props.picture} />
+        <p>{this.props.about}</p>
+      </div>
+      
     )
   }
 }
